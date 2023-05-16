774f5c2b9d9de410861cb089c280b3f4d967eb74
rachelschw <rachels4130@gmail.com>
2023-05-16 16:36:07 +0300

    second commit

--- index.js
+++ index.js
@@ -1,36 +1,30 @@
 import express from 'express'
-import require from 'api'
+import cors from "cors";
+import axios from "axios";
 
-const app=express()
+const app=express();
+app.use(cors());
 const port = 3000
 
-const sdk = require('api','@render-api/v1.0#dnrc1ulf088q9j');
+const options = {
+  method: 'GET',
+ url: 'https://api.render.com/v1/services?limit=20',
+  headers: {
+    accept: 'application/json',
+    authorization: 'Bearer rnd_IYfuLxO4VBurXgp0UfqDv6lyFN6a'
+  }
+};
+app.get('/',(req,res)=>{res.send("hi")})
 
-sdk.auth('rnd_IYfuLxO4VBurXgp0UfqDv6lyFN6a');
-sdk.getServices({limit: '20'})
-  .then(({ data }) => console.log(data))
-  .catch(err => console.error(err));
-
-// const axios = require('axios');
-// const options = {
-//   method: 'GET',
-//  url: 'https://api.render.com/v1/services?limit=20',
-//   headers: {
-//     accept: 'application/json',
-//     authorization: 'Bearer rnd_IYfuLxO4VBurXgp0UfqDv6lyFN6a'
-//   }
-// };
-app.get('/hi',(req,res)=>{res.send("hi")})
-
-app.get('/i',(req,res)=>{res.send(sdk)})
-// app.get('/i',(req,res)=>{res.send(
-//      axios.request(options)
-//     .then(function (response) {
-//       console.log(response.data);
-//     })
-//     .catch(function (error) {
-//       console.error(error);
-//     })
-//     )})
+app.get('/i',(req,res)=>{
+     axios.request(options)
+    .then(function (response) {
+      console.log(response.data);
+      res.send(response.data);
+    })
+    .catch(function (error) {
+      console.error(error);
+    })
+  })
 
 app.listen(port,()=>{console.log(`example app listening on http://localhost:${port}`);})
